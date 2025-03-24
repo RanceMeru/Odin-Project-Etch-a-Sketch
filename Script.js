32a0ec96d6187d42b26543b5bcaebbf081e1d87b
RanceMeru <johgu.829@gmail.com>
2025-03-24 13:02:54 -0700

    Html, CSS and Javascript elements and grid creation
    In html I created new Ids for the containers, paragraph and the grid containers then in javascript to have the element creation to make a 16x16 grid and in CSS giving the grid a broder to make it lightly visible for the user and to show the progression.

--- Script.js
+++ Script.js
@@ -5,3 +5,29 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
 container.appendChild(content);
+
+//exercise from DOM manipulation
+//makes red text in the <p>
+
+
+const paragraph = document.createElement("p");
+paragraph.textContent="Hey Im red";
+paragraph.style.color = "red";
+container.appendChild(paragraph);
+
+
+//create a 16x16 div grid
+//make a for loop to constantly make a grid up to 16x16
+const gridContainer = document.getElementById('grid-container');
+
+for(let i = 0; i < 16 *16; i++){
+    const gridDiv = document.createElement('div');
+    gridDiv.classList.add('grid-item');
+
+
+}
+
+
+
+
+//using javscript you have to make 6 grid layout on the html file 
